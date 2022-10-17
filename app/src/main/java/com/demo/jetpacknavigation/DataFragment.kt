525603eb0f418a545f736dc330a5d8c18ba93ef0
Renz Manacmol <renz@backbase.com>
2022-10-17 16:20:34 +0800

    init commit

---
+++ app/src/main/java/com/demo/jetpacknavigation/DataFragment.kt
@@ -0,0 +1,24 @@
+package com.demo.jetpacknavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class DataFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_data, container, false)
+        val button = view.findViewById<Button>(R.id.button2)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_dataFragment_to_homeFragment)
+        }
+        return view
+    }
+}
